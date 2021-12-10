80a2c5728c5ce7ebcda6442893d7e9b7997d3839
Salman Prottoy <salman.prottoy@gmail.com>
2021-12-10 19:05:34 +0600

    updated

--- app.py
+++ app.py
@@ -1,28 +1,13 @@
-from flask import Flask, jsonify, request, make_response
-import jwt
 import datetime
+import json
 from functools import wraps
+
+import jwt
+import mysql.connector
+from flask import Flask, jsonify, request, make_response, redirect
 from flask_swagger_ui import get_swaggerui_blueprint
 
-# from routes import request_api
 app = Flask(__name__)
-
-### swagger specific ###
-SWAGGER_URL = '/swagger'
-API_URL = '/static/swagger.json'
-SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': "Flask Restful OpenAPI"
-    }
-)
-app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
-### end swagger specific ###
-
-
-# app.register_blueprint(request_api.get_blueprint())
-
 
 app.config['SECRET_KEY'] = 'mysecretkey'
 
@@ -47,16 +32,111 @@ def token_required(f):
 
 
 # Unprotected Route and function
-@app.route('/unprotected')
-def unprotected():
-    return jsonify({'message': 'Anyone can view this.'})
+@app.route('/')
+def index():
+    return redirect('/login')
+
+
+@app.route('/data', methods=['GET'])
+def house():
+    hotel_data = []
+
+    db_connector = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="test"
+    )
+
+    title = request.args.get('title')
+    bedroom = request.args.get('bedroom')
+    sleep = request.args.get('sleeps')
+    bathroom = request.args.get('bathroom')
+    price = request.args.get('price')
+    location = request.args.get('location')
+
+    cursor = db_connector.cursor()
+    # sleep  	bedroom  	bathroom  	price
+    sql = "SELECT * FROM family_friendly_rentals WHERE "
+    valudata = 0
+
+    if (title != None):
+        if (sql == 'SELECT * FROM family_friendly_rentals WHERE '):
+            sql = sql + "Title LIKE " + f"'%{title}%'"
+        else:
+            sql = sql + "AND Title LIKE " + f"'%{title}%'"
+
+    if (location != None):
+        if (sql == 'SELECT * FROM family_friendly_rentals WHERE '):
+            sql = sql + "Location LIKE " + f"'%{location}%'"
+        else:
+            sql = sql + "AND Location LIKE " + f"'%{location}%'"
+    if (bedroom != None):
+        if (sql == 'SELECT * FROM family_friendly_rentals WHERE '):
+            sql = sql + "Bedroom LIKE " + f"'%{bedroom}%'"
+        else:
+            sql = sql + "AND Bedroom LIKE " + f"'%{bedroom}%'"
+    if (bathroom != None):
+        if (sql == 'SELECT * FROM family_friendly_rentals WHERE '):
+            sql = sql + "Bathroom LIKE " + f"'%{bathroom}%'"
+        else:
+            sql = sql + "AND Bathroom LIKE" + f"'%{bathroom}%'"
+    if (sleep != None):
+        if (sql == 'SELECT * FROM family_friendly_rentals WHERE '):
+            sql = sql + "Sleeps LIKE " + f"'%{sleep}%'"
+        else:
+            sql = sql + "AND Sleeps LIKE" + f"'%{sleep}%'"
+    if (price != None):
+        if (sql == 'SELECT * FROM family_friendly_rentals WHERE '):
+            sql = sql + "Price LIKE" + f"'$%{price}%'"
+        else:
+            sql = sql + "AND Price LIKE " + f"'$%{price}%'"
+
+
+    query = sql
+    print(query)
+    cursor.execute(query)
+
+    results = cursor.fetchall()
+
+    for x in results:
+        data = {
+            "id": x[0],
+            "Title": x[1],
+            "Location": x[2],
+            "Sleeps": x[3],
+            "Bedrooms": x[4],
+            "Bathrooms": x[5],
+            "Images": {
+                "Image1": x[6][1:-1],
+                "Image2": x[7][1:-1],
+                "Image3": x[8][1:-1],
+            },
+            "Price": x[9],
+
+        }
+        hotel_data.append(data)
+    hotel_data.sort(key=lambda x: x["Price"])
+    houseJson = json.dumps(hotel_data, indent=4)
+    print(houseJson)
+    return houseJson
 
 
 # Protected Route and function
 @app.route('/protected')
 @token_required
 def protected():
-    return jsonify({'message': 'Only available to people with valid tokens.'})
+    SWAGGER_URL = '/swagger'
+    API_URL = '/static/swagger.json'
+    SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config={
+            'app_name': "Restful OpenAPI"
+        }
+    )
+    app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+    return redirect("/swagger", code=302)
 
 
 # Login Route and function
@@ -65,17 +145,14 @@ def login():
     auth = request.authorization
 
     if auth and auth.password == '1234':
-        token = jwt.encode({'user': auth.username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=50)},
+        token = jwt.encode({'user': auth.username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=100)},
                            app.config['SECRET_KEY'])
-        return jsonify({'token': token})
+        return jsonify(
+            {'token': token, 'msg': 'use the token using token-url for accessing swagger',
+             'token-url': f'/protected?token={token}'})
 
     return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm:"Login Required"'})
 
-
-# @app.route('/static/<path:path>')
-# def send_static(path):
-#     return send_from_directory('static',path)
-# app.register_blueprint(requset_api.get.get_blueprint())
 
 if __name__ == "__main__":
     app.run(debug=True)
